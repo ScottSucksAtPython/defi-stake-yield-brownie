4a379201a38b4bde155df7c87690421833034eb0
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-04-24 19:16:42 -0400

    updated python scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,10 +6,12 @@
 # Modification History
 # 04-20-2022 SRK Project Created.
 # 04-22-2022 SRK Documentation Updated.
+# 04-24-2022 SRK Added update_frontend function.
 
 # -------------------------------- Tasks ----------------------------------- #
 
 # ------------------------------- Resources -------------------------------- #
+from turtle import update
 from scripts.helpful_scripts import (
     get_account,
     get_contract,
@@ -18,17 +20,22 @@ from scripts.helpful_scripts import (
 )
 from brownie import config, network, DappToken, TokenFarm
 from web3 import Web3
+import yaml, json, os, shutil
 
 # ------------------------------- Variables -------------------------------- #
 KEPT_BALANCE = Web3.toWei(100, "ether")
 
 # ------------------------------ Functions --------------------------------- #
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(update_front_end=False):
     """
     This function deploys the TokenFarm, DappToken and other associated
     contracts to the blockchain. Dapp tokens are transferred into the Token
     Farm contract. Dapp, Weth and Fau tokens are added to the list of tokens
     which can be staked to the Token Farm.
+
+    Arguments:
+        update_front_end (bool) - Set as true if you want the frontend files to be
+        updated during deployment.
 
     Returns:
         token_farm - The Token Farm contract object.
@@ -53,6 +60,8 @@ def deploy_token_farm_and_dapp_token():
         weth_token: get_contract("eth_usd_price_feed"),
     }
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    if update_front_end:
+        update_frontend()
     return token_farm, dapp_token
 
 
@@ -78,9 +87,24 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
     return token_farm
 
 
+def update_frontend():
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+    with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+        json.dump(config_dict, brownie_config_json)
+    print("Frontend Updated!")
+
+
+def copy_folders_to_front_end(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
+
 # ----------------------------- Main Function ------------------------------ #
 def main():
     """
     This function will be automatically excecuted by brownie when this script is run.
     """
-    deploy_token_farm_and_dapp_token()
+    deploy_token_farm_and_dapp_token(update_front_end=True)
---
+++ scripts/update_front_end.py
@@ -0,0 +1,21 @@
+# ------------------------------ Documentation ------------------------------ #
+# Module:  update_front_end.py
+# When run this will copy information from deployments to our front_end folder.
+#
+#
+# Modification History
+# 04-24-2022 SRK Created.
+
+# -------------------------------- Tasks ----------------------------------- #
+
+# ------------------------------- Resources -------------------------------- #
+from scripts.deploy import update_frontend
+
+# ------------------------------- Variables -------------------------------- #
+
+# ------------------------------ Functions --------------------------------- #
+
+
+# ----------------------------- Main Function ------------------------------ #
+def main():
+    update_frontend()
